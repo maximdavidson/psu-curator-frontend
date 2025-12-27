ee0b0bf1221b3cbfc58c7d06d6e2d948c3579e29
Maxim Davidson <maxd02122004@gmail.com>
2025-12-28 00:07:22 +0300

    feat(PG-02): registration form

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import { useEffect, useState } from "react";
 import "./App.css";
+import { RegistrationPage } from "./pages/registration";
 
 function App() {
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const resp = await fetch("http://localhost:8080/WeatherForecast");
-        const newData = await resp.json();
-
-        setData(newData);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  });
   return (
     <>
-      <p>hello</p>
-      {data}
+      <RegistrationPage />
     </>
   );
 }
---
+++ src/pages/registration/components/form/form.component.tsx
@@ -0,0 +1,104 @@
+import { useState, type ChangeEvent } from "react";
+import styles from "./styles.module.scss";
+import { registrationSchema } from "../../../../validation/formValidation";
+import { ValidationError } from "yup";
+
+export const FormComponent = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState<Record<string, string>>({});
+
+  const isDisabled =
+    isSubmitting || !email || !password || !!error.email || !!error.password;
+
+  const validateField = async (field: "email" | "password", value: string) => {
+    try {
+      await registrationSchema.pick([field]).validate({ [field]: value });
+      setError((prev) => ({ ...prev, [field]: "" }));
+    } catch (err) {
+      if (err instanceof ValidationError) {
+        setError((prev) => ({ ...prev, [field]: err.message }));
+      }
+    }
+  };
+
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setEmail(value);
+    validateField("email", value);
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setPassword(value);
+    validateField("password", value);
+  };
+
+  const handleSubmit = async () => {
+    if (!email || !password || error.email || error.password) return;
+
+    setIsSubmitting(true);
+    try {
+      console.log("Отправка данных:", { email, password });
+      // ToDo
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const handleLoginRedirect = () => {
+    // ToDo
+  };
+
+  return (
+    <div className={styles.formWrapper}>
+      <h1 className={styles.title}>Зарегистрироваться</h1>
+
+      <form className={styles.form}>
+        <div className={styles.field}>
+          <label className={styles.label}>Почта</label>
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            className={styles.input}
+          />
+          <div className={styles.error}>{error.email || ""}</div>
+        </div>
+
+        <div className={styles.field}>
+          <label className={styles.label}>Пароль</label>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            className={styles.input}
+          />
+          <div className={styles.error}>{error.password || ""}</div>
+          <a className={styles.forgotPassword}>Забыли пароль?</a>
+        </div>
+
+        <button
+          type="button"
+          onClick={handleSubmit}
+          className={styles.submitButton}
+          disabled={isDisabled}
+        >
+          {isSubmitting ? "Отправка..." : "Зарегистрироваться"}
+        </button>
+      </form>
+
+      <div className={styles.footer}>
+        <span>У вас есть аккаунт?</span>
+        <button
+          type="button"
+          onClick={handleLoginRedirect}
+          className={styles.link}
+        >
+          Войти
+        </button>
+      </div>
+    </div>
+  );
+};
