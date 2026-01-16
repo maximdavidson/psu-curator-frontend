cd0b5c8d9a6c96d408e22ead46dae8f5c2bfae3d
Zahar <zaharrubanik2007@gmail.com>
2026-01-17 02:29:10 +0300

    feat: add login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { RegistrationPage } from "./pages/registration";
+import { LoginationPage } from "./pages/registration";
 
 function App() {
   return (
     <>
-      <RegistrationPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/register" />} />
+          <Route path="/login" element={<LoginationPage />} />
+          <Route path="/register" element={<RegistrationPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/registration/index.tsx
+++ src/pages/registration/index.tsx
@@ -1,10 +1,19 @@
 import { FormComponent } from "./components/form/form.component";
+import { LoginComponent } from "./components/form/login.componet";
 import styles from "./styles.module.scss";
 
 export const RegistrationPage = () => {
   return (
     <div className={styles.registrationPage}>
       <FormComponent />
+    </div>
+  );
+};
+
+export const LoginationPage = () => {
+  return (
+    <div className={styles.registrationPage}>
+      <LoginComponent />
     </div>
   );
 };
